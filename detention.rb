44ce34b51e39aae4118b92e2674df9aa264ced34
Juliet Stern <julesstern@aol.com>
2016-06-24 17:53:57 +0000

    team: @julietstern @alexappel

--- detention.rb
+++ detention.rb
@@ -1,1 +1,21 @@
-#Code your detention class here
+class Detention
+  attr_accessor :location, :time, :grumpy_teacher, :activity, :students, :add_student
+  def initialize
+    @location = "Room 301"
+    @time = "3:00 PM"
+    @grumpy_teacher = "John Schmidt"
+    @activity = "collective punishment"
+    @students = []
+  end
+
+  def add_student(name)
+    @students << name
+  end
+
+  def remove_student(name)
+    @students.delete(name)
+  end
+
+
+
+end
